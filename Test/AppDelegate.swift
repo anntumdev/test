f94c7cd3912de1c9a7190c7a3c7f980901f26774
annrheon <anechka596@mail.ru>
2023-03-04 20:08:37 +0200

    Init project

---
+++ Test/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Test
+//
+//  Created by Anna on 04.03.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
